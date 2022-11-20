bbf54ffe7758d9d12ff02e12170fe337f482a3d4
f4r00t7 <102198233+f4r00t7@users.noreply.github.com>
2022-11-20 00:24:40 -0300

    Add files via upload

---
+++ iplocation/iploc.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+import sys
+
+try:
+    #Pegando ip 
+    ip = sys.argv[1]
+
+    if ip:
+        url = f"http://ip-api.com/xml/{ip}"
+
+        #iniciando o request
+        request = urlopen(url)
+        data = request.read().decode()
+
+        #convertendo string api, para DICT (dicionário)
+        data = eval(data)
+
+        for i in data:
+            print(f"{i} == {data[i]}")
+
+except Exception as ex:
+    print(f"error: {ex}")
